04b5f2acc26aa8a8ad529e673b3d20c38675ec81
RoviMedina <rosvias_medina@dlsu.edu.ph>
2026-06-27 19:13:42 +0800

    bug fix

--- app/src/main/java/com/mobdeve/s15/medina_aguado_pantaleon/mobdeve_mco/MainActivity.kt
+++ app/src/main/java/com/mobdeve/s15/medina_aguado_pantaleon/mobdeve_mco/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        val btnLogin = findViewById<Button?>(R.id.btnLogin)
+        val btnLogin = findViewById<Button>(R.id.btnLogin)
         btnLogin.setOnClickListener {
             val intent = Intent(this, DashboardActivity::class.java)
             startActivity(intent)
